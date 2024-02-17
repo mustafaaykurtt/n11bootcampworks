48dfc8a524ef4d044f677f04cbb4ae32c2f0f6a2
mustafa <mustafaaykurt35@gmail.com>
2024-02-17 20:00:13 +0300

    First commit - project config

---
+++ src/main/java/com/mustafaworks/n11secondweek/N11secondweekApplication.java
@@ -0,0 +1,13 @@
+package com.mustafaworks.n11secondweek;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class N11secondweekApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(N11secondweekApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/mustafaworks/n11secondweek/N11secondweekApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mustafaworks.n11secondweek;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class N11secondweekApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
